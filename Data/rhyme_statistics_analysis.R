5d3881cf3acff8e1662f01e88d08b6090f5eeeca
rkimelman <rkimelman@wustl.edu>
2022-04-25 17:04:00 -0400

    add probabilites

--- Data/rhyme_statistics_analysis.R
+++ Data/rhyme_statistics_analysis.R
@@ -100,6 +100,51 @@ indices15 <- which(nonrhymes15== TRUE)
 participant15$correct_answers <- 0
 participant15$correct_answers[indices15] <- 1
 
+nonrhymes16 <- grepl("nonrhyme", participant16$stimulus)
+indices16 <- which(nonrhymes16== TRUE)
+participant16$correct_answers <- 0
+participant16$correct_answers[indices16] <- 1
+
+nonrhymes17 <- grepl("nonrhyme", participant17$stimulus)
+indices17 <- which(nonrhymes17== TRUE)
+participant17$correct_answers <- 0
+participant17$correct_answers[indices17] <- 1
+
+nonrhymes18 <- grepl("nonrhyme", participant18$stimulus)
+indices18 <- which(nonrhymes18== TRUE)
+participant18$correct_answers <- 0
+participant18$correct_answers[indices18] <- 1
+
+nonrhymes19 <- grepl("nonrhyme", participant19$stimulus)
+indices19 <- which(nonrhymes19== TRUE)
+participant19$correct_answers <- 0
+participant19$correct_answers[indices19] <- 1
+
+nonrhymes20 <- grepl("nonrhyme", participant20$stimulus)
+indices20 <- which(nonrhymes20== TRUE)
+participant20$correct_answers <- 0
+participant20$correct_answers[indices20] <- 1
+
+nonrhymes21 <- grepl("nonrhyme", participant21$stimulus)
+indices21 <- which(nonrhymes21== TRUE)
+participant21$correct_answers <- 0
+participant21$correct_answers[indices21] <- 1
+
+nonrhymes22 <- grepl("nonrhyme", participant22$stimulus)
+indices22 <- which(nonrhymes22== TRUE)
+participant22$correct_answers <- 0
+participant22$correct_answers[indices22] <- 1
+
+nonrhymes23 <- grepl("nonrhyme", participant23$stimulus)
+indices23 <- which(nonrhymes23== TRUE)
+participant23$correct_answers <- 0
+participant23$correct_answers[indices23] <- 1
+
+nonrhymes24 <- grepl("nonrhyme", participant24$stimulus)
+indices24 <- which(nonrhymes24== TRUE)
+participant24$correct_answers <- 0
+participant24$correct_answers[indices24] <- 1
+
 score_1 <- length(which(participant1$response == participant1$correct_answers))
 score_2 <- length(which(participant2$response == participant2$correct_answers))
 score_3 <- length(which(participant3$response == participant3$correct_answers))
@@ -115,3 +160,27 @@ score_12 <- length(which(participant12$response == participant12$correct_answers
 score_13 <- length(which(participant13$response == participant13$correct_answers))
 score_14 <- length(which(participant14$response == participant14$correct_answers))
 score_15 <- length(which(participant15$response == participant15$correct_answers))
+score_16 <- length(which(participant16$response == participant16$correct_answers))
+score_17 <- length(which(participant17$response == participant17$correct_answers))
+score_18 <- length(which(participant18$response == participant18$correct_answers))
+score_19 <- length(which(participant19$response == participant19$correct_answers))
+score_20 <- length(which(participant20$response == participant20$correct_answers))
+score_21 <- length(which(participant21$response == participant21$correct_answers))
+score_22 <- length(which(participant22$response == participant22$correct_answers))
+score_23 <- length(which(participant23$response == participant23$correct_answers))
+score_24 <- length(which(participant24$response == participant24$correct_answers))
+data <- c(score_1, score_2, score_3, score_4, score_5, score_6, score_7, score_8, score_9, score_10,score_11,score_12, score_13, score_14, score_15, score_16, score_17, score_18, score_19, score_20, score_21, score_22, score_23, score_24)
+
+EndTwoVowel_Ps <- c(0.00133705, 0.00004797, 0.00073205, 0.00376915, 0.00074415, 0.00076629,
+                    0.00076629, 0.00075383, 0.00074415, 0.00133705, 0.00076629, 0.00008619,
+                    0.00004797, 0.00376915)
+
+EndThreeVowel_Ps <- c(8.02E-07, 5.80E-07, 5.95E-09, 1.63098E-07, 5.8525E-09, 5.94924E-09,
+                      1.64451E-09, 2.93994E-06, 2.98853E-06, 8.26098E-07, 1.60446E-07,1.63098E-07,
+                      8.26098E-07, 1.0429E-06, 1.06014E-06, 5.36964E-06, 9.07888E-08, 5.8525E-09,
+                      5.10218E-09, 9.07888E-08, 2.92625E-08, 2.55109E-08, 1.84579E-08,5.94924E-09,
+                      5.18652E-09, 5.8525E-09, 5.94924E-09, 1.64451E-09, 5.10218E-09, 9.50353E-08,
+                      2.62699E-08, 5.10218E-09, 5.18652E-09, 2.62699E-08, 3.31642E-08, 3.37124E-08,
+                      1.65798E-07, 9.12134E-06, 2.93994E-06, 2.56302E-06, 4.56067E-05, 1.46997E-05,
+                      1.28151E-05, 9.27211E-06, 2.98853E-06, 2.60539E-06, 2.93994E-06, 2.98853E-06,
+                      8.26098E-07, 9.349E-08)
